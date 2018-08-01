d54aecdfccdd7fa382b1f68d94ab7bde78c1605a
Christia T <christiatidwell@bellsouth.net>
2018-08-01 22:43:10 +0000

    automatically backed up by learn

--- daily_recipe/lib/daily_recipe.rb
+++
@@ -1,7 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-
-require_relative './daily_recipe/version'
-require_relative './daily_recipe/cli'
-require_relative './daily_recipe/recipe'
--- daily_recipe/lib/daily_recipe/version.rb
+++
@@ -1,3 +0,0 @@
-module DailyRecipe
-  VERSION = "0.1.0"
-end
