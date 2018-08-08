93b567b484fd01bdcc7e958d16f1cfb0db9a8a76
Christia T <christiatidwell@bellsouth.net>
2018-08-08 13:40:18 +0000

    improved spacing in code and scraping

--- lib/daily_recipe/recipe.rb
+++ lib/daily_recipe/recipe.rb
@@ -17,26 +17,26 @@ attr_accessor :title, :link, :description
   end
 
   # scraping recipes and information from food and wine
-  #corrected spacing
+  #improved spacing in code and scraping
   def self.scrape_foodandwine
     doc = Nokogiri::HTML(open("https://www.foodandwine.com/special-diets/vegan/vegan-recipes"))
 
     recipe = self.new
     recipe.title = doc.search("a.headline-link").text.strip.split(":")
     recipe.link = doc.css(".caption.margin-24-bottom a").attribute("href").value
-    recipe.description = doc.css(".caption.margin-24-bottom").text.strip.split("\r\n")
+    recipe.description = doc.css(".caption.margin-24-bottom").text.strip.split("\r\n").each(&:lstrip!)
     recipe
 
   end
 
   #scraping real simple web page for recipes and information
-  #corrected spacing
+  #improved spacing in code and scraping
   def self.scrape_realsimple
 
     doc = Nokogiri::HTML(open("https://www.realsimple.com/food-recipes/recipe-collections-favorites/healthy-meals/vegetarian-recipes"))
 
     recipe = self.new
-    recipe.title = doc.search("div.media-body.clearfix h2").text.strip.split("\n").each_slice(2).map(&:first)
+    recipe.title = doc.search("div.media-body.clearfix h2").text.strip.split("\n").each_slice(2).map(&:first).each(&:lstrip!)
     recipe.link = "https://www.realsimple.com/food-recipes/recipe-collections-favorites/healthy-meals/vegetarian-recipes"
     recipe.description = doc.css(".caption.margin-24-bottom").text.strip.split("\r\n")
     recipe
